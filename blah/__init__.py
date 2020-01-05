ba7dd5aa923d739cadf68de63f5b72819f0cbd6d
Ellen Marie Dash <me@duckie.co>
2020-01-05 12:04:37 -0500

    ????

--- blah/__init__.py
+++ blah/__init__.py
@@ -1,20 +1,9 @@
 """An interface for set-top boxes.
 """
 
-import sys
-
 #: Maintainer.
 __author__ = "Ellen Marie Dash"
 
 #: The current version.
 #: This project follows the Semantic Versioning convention; see https://semver.org/
 __version__ = "0.0.1"
-
-def main(args=None):
-    """???
-    """
-
-    if args is None:
-        args = sys.argv[1:]
-
-    print("aooo")
---
+++ blah/interface.py
@@ -0,0 +1,14 @@
+import kivy
+kivy.require('1.0.7')
+
+from kivy.app import App
+
+
+class InterfaceApp(App):
+    # This is required when the app is installed via pip.
+    # The value should be the directory this file is in.
+    kv_directory = 'blah'
+
+
+def main(_args=None):
+    InterfaceApp().run()
