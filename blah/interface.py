2ab9aa821d401e79580b5329e1d0455559878956
Ellen Marie Dash <me@duckie.co>
2020-01-05 12:56:03 -0500

    bleh

--- blah/interface.py
+++ blah/interface.py
@@ -9,14 +9,10 @@ class InterfaceApp(App):
     # The value should be the directory this file is in.
     kv_directory = 'blah'
 
-    def on_start(self):
-        print('owo')
-        for child in self.root_window.children[0].children:
-            print(child, child.height, child.width)
-        #row_default_height = 
 
 def main(_args=None):
     InterfaceApp().run()
+
 
 if __name__ == '__main__':
     main()
