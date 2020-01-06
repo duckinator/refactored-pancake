0c079125fcdac845e83d3254fce4269f2d487d7f
Ellen Marie Dash <me@duckie.co>
2020-01-06 18:11:18 -0500

    ?!

---
+++ blah/apps.py
@@ -0,0 +1,17 @@
+from subprocess import check_call as run
+
+
+def firefox():
+    pass
+
+
+def firefox_incognito():
+    pass
+
+
+def youtube():
+    pass
+
+
+def plex():
+    pass
--- blah/interface.py
+++ blah/interface.py
@@ -1,8 +1,11 @@
+import re
+
 import kivy
 kivy.require('1.0.7')
 
 from kivy.app import App
 
+from . import apps
 
 class InterfaceApp(App):
     # This is required when the app is installed via pip.
@@ -13,9 +16,22 @@ class InterfaceApp(App):
         for child in self.root.children[0].children:
             child.bind(on_press=self.start)
 
+    def start(self, button):
+        # Lowercase everything, replace spaces with underscore.
+        name = button.text.lower().replace(' ', '_')
+        # Replace anything that's not alphanumeric with underscores.'
+        name = re.sub(r'[^a-z0-9]', '_', name)
+        # Replace repeated underscores with one underscore.
+        name = re.sub(r'_+', '_', name)
+        # Remove leading and trailing underscores.
+        name = re.sub(r'^_|_$', '', name)
+        error_callback = lambda: self.error(name)
+        fn = getattr(apps, name, error_callback)
+        fn()
+
     @staticmethod
-    def start(button):
-        print(button.text)
+    def error(name):
+        print('NO SUCH FUNCTION: {}'.format(name))
 
 
 def main(_args=None):
