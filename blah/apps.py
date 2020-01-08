ee3863f981fee9e42b9eb05134950842d315dbd9
Ellen Marie Dash <me@duckie.co>
2020-01-07 23:05:25 -0500

    whooooooo

--- blah/apps.py
+++ blah/apps.py
@@ -1,19 +1,17 @@
-from subprocess import check_call as run
-
 PROFILE_ARGS = ['-P', 'secondary']
 
 
 def firefox():
-    run(['firefox', *PROFILE_ARGS])
+    return ['firefox', *PROFILE_ARGS]
 
 
 def firefox_private_window():
-    run(['firefox', *PROFILE_ARGS, '--private-window'])
+    return ['firefox', *PROFILE_ARGS, '--private-window']
 
 
 def youtube():
-    run(['firefox', *PROFILE_ARGS, '--kiosk', 'https://youtube.com'])
+    return ['firefox', *PROFILE_ARGS, '--kiosk', 'https://youtube.com']
 
 
 def plex():
-    run(['firefox', *PROFILE_ARGS, '--kiosk', 'https://plex.tv'])
+    return ['firefox', *PROFILE_ARGS, '--kiosk', 'https://plex.tv']
--- blah/interface.py
+++ blah/interface.py
@@ -11,13 +11,41 @@ kivy.require('1.0.7')
 from kivy.app import App
 
 import re
+import subprocess
 
 from . import apps
+
+
+# Handles exactly one child process.
+# If you start one before the other one has exited, it gets replaced.
+class ProcessHandler:
+    def __init__(self):
+        self.child = None
+
+    def start(self, args):
+        # If there's an old child process, kill it.
+        if self.child and not self.child.poll():
+            print("[ProcessHandler] Termininating old process.")
+            self.child.kill()
+
+        self.child = subprocess.Popen(args)
+        return self.child
+
+    def stop(self):
+        if not self.child:
+            return
+        if self.child.poll() is not None:
+            return
+        self.child.kill()
+
 
 class InterfaceApp(App):
     # This is required when the app is installed via pip.
     # The value should be the directory this file is in.
     kv_directory = 'blah'
+
+    # Manages a single process for the various apps.
+    app = ProcessHandler()
 
     def on_start(self):
         for child in self.root.children[0].children:
@@ -33,12 +61,13 @@ class InterfaceApp(App):
         # Remove leading and trailing underscores.
         name = re.sub(r'^_|_$', '', name)
         error_callback = lambda: self.error(name)
-        fn = getattr(apps, name, error_callback)
-        fn()
+        app_fn = getattr(apps, name, None)
+        if app_fn is None:
+            print('NO SUCH FUNCTION: {}'.format(name))
+            return
 
-    @staticmethod
-    def error(name):
-        print('NO SUCH FUNCTION: {}'.format(name))
+        command = app_fn()
+        self.app.start(command)
 
 
 def main(_args=None):
